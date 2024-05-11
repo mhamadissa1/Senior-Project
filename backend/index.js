2fba561eec676c3d533b5e3252dcc0981e0664f3
Monah-Haidar <mnhaidar01@gmail.com>
2024-05-12 01:21:49 +0300

    added api to fetch latest prices of crypto currencies

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
+import 'dotenv/config';
+
+
 
 const app = express();
 const port = 3000;
+
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -12,12 +18,39 @@ app.get("/", (req, res)=>{
 })
 
 
-app.post("/register", (req, res)=>{
-    res.send(req.ip);
-})
-
-
-
+app.get('/markets', async (req, res) => {
+    try {
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+            headers: {
+                'X-CMC_PRO_API_KEY': process.env.CMC_KEY,
+                'Accept': 'application/json'
+            },
+            params: {
+                start: 1,
+                limit: 1,
+                convert: 'USD'
+            }
+        });
+        res.json(response.data);
+        
+    } catch (error) {
+        if (error.response) {
+            // Request made and server responded
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+            res.status(504).send("The request was made but no response was received");
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+            res.status(500).send(error.message);
+        }
+    }
+});
 
 
 
